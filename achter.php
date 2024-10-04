340d092cef9d4e6a7fbd078847282b553adf22ae
maarten <maartensitskoorn0@gmail.com>
2024-10-04 22:28:28 +0200

    het werkt

---
+++ achter.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>achter</title>
+</head>
+<body>
+<a href="http://localhost:63342/untitled/index.php?maart=voor"> <img src="achterkant.jpeg"> </a>
+</body>
+</html>
---
+++ voor.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>voor</title>
+</head>
+<body>
+<a href="http://localhost:63342/untitled/index.php?maart=achter"> <img src="voorkant.jpeg"> </a>
+</body>
+</html>
