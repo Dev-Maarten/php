327dd7deba0ea551e04f4bd6c779e8a03642f9df
Dev-Maarten <maartensitskoorn0@gmail.com>
2024-12-03 10:59:23 +0100

    Delete achter.php

--- achter.php
+++
@@ -1,9 +0,0 @@
-<!DOCTYPE html>
-<html>
-<head>
-    <title>achter</title>
-</head>
-<body>
-<a href="http://localhost:63342/untitled/index.php?maart=voor"> <img src="achterkant.jpeg"> </a>
-</body>
-</html>
