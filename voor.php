a8d5fcad33157d7bbbfa616eb684cbf6faecf5ef
Dev-Maarten <maartensitskoorn0@gmail.com>
2024-12-03 10:59:56 +0100

    Delete voor.php

--- voor.php
+++
@@ -1,9 +0,0 @@
-<!DOCTYPE html>
-<html>
-<head>
-    <title>voor</title>
-</head>
-<body>
-<a href="http://localhost:63342/untitled/index.php?maart=achter"> <img src="voorkant.jpeg"> </a>
-</body>
-</html>
